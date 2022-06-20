b00ba35a5085421e54fbd9ed843c98ccf269bf99
一 丁 <970102437@qq.com>
2022-06-20 22:30:14 +0800

    MyBatisPlus CRUD

--- MyBatisPlusFirst/src/test/java/com/cqu/MyBatisPlusFirstApplicationTests.java
+++ MyBatisPlusFirst/src/test/java/com/cqu/MyBatisPlusFirstApplicationTests.java
@@ -13,6 +13,32 @@ class MyBatisPlusFirstApplicationTests {
     @Autowired
     private UserDao userDao;
     @Test
+    void testSave(){
+        User user = new User();
+        user.setName("黑马程序员");
+        user.setAge(16);
+        user.setPassword("159753");
+        user.setTel("123456789");
+        userDao.insert(user);
+    }
+    @Test
+    void testUpdate(){
+        User user = new User();
+        user.setId(4L);
+        user.setName("Tom666");
+        user.setTel("wowowow");
+        userDao.updateById(user);
+    }
+    @Test
+    void testDelete(){
+        userDao.deleteById(1538890099570364418L);
+    }
+    @Test
+    void testGetById(){
+        User user = userDao.selectById(2L);
+        System.out.println(user);
+    }
+    @Test
     void TestGetAll() {
         List<User> user = userDao.selectList(null);
         System.out.println(user);
